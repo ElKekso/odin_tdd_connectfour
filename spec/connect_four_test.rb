5945569d7d9fb1a26a490057b633b74bee10f139
CorvinZloch <zloch.corvin@gmail.com>
2025-09-03 17:42:41 +0200

    Add first Test and Player.rb

---
+++ spec/connect_four_test.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/connect_four.rb'
+require_relative '../lib/player.rb'
+
+describe ConnectFour do
+
+  let(:player1) { Player.new('Aaron') }
+  let(:player2) { Player.new('Marlene') }
+
+    #Looping Script Method
+    describe '#start_game' do
+        
+    end
+
+    describe '#player_move' do
+        subject(:game_move) { described_class.new }
+        it 'changes the board' do
+            expect { game.player_move(player1,2) }.to change { game.instance_variable_get(:board)[1][0] }.to(1)
+        end
+    end
+end
